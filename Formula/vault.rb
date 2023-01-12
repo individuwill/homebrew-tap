9721c3543f873150162f750cab50d2e9b37b426f
Will Smith <Will.Smith@recursionpharma.com>
2023-01-11 16:36:06 -0800

    add bottle back

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -17,6 +17,16 @@ class Vault < Formula
     regex(%r{href=.*?v?(\d+(?:\.\d+)+)/?["' >]}i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "edd602466cb5dbe92e5802d853bfbe1bbf5defc6501522dc6c50696126e04c54"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6c622af5f77c7e085ed67e14e707a5b938fa43943d273085be1b78a55fe4e560"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a9a85ec8faa1009f2cad1f3fbe3308c08fa3e9da2f9e104aee8a0c24f924fa99"
+    sha256 cellar: :any_skip_relocation, ventura:        "91fa68e19b2c80d7f76f68e3b41f97d6a344e866bd342910412e8677ed653a84"
+    sha256 cellar: :any_skip_relocation, monterey:       "ab6db09e674138c955e08930647c95ef00ebef3f54b93b7d22ab2d92507a1778"
+    sha256 cellar: :any_skip_relocation, big_sur:        "25df1c9f86e8759f6c02186078ae5b6b83262c296efe4b7c7d7d1016246a9703"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cd10bf75971478cb44dc12960843556a159fc94c68925feeb59c893753a63458"
+  end
+
   depends_on "go" => :build
   depends_on "gox" => :build
   depends_on "node@18" => :build
